7f9ea90410a952da63439bb8d6c6d9a60c704669
Apurva Shah <shahh.apurva@gmail.com>
2022-12-21 03:02:01 -0800

    Added homepage updates with API access to GraphCMS

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
+import { printSchema } from 'graphql';
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services'
 
-const posts = [
-  {title: 'React Application', excerpt: 'We teach you how to create the react application'},
-  {title: 'Angular Application', excerpt: 'We teach you how to create the angular application'},
-];
 
-export default function Home() {
+
+export default function Home({ posts }) {
   return (
-    <div className='container mx-auto px-10 mb-8 bg-gray-300'>
+    <div className='container mx-auto px-10 mb-8'>
       <Head>
         <title>Apurva Shah's Blog</title>
         <meta name="description" content="Sharing my journey through life!" />
@@ -16,19 +16,23 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post) => <PostCard post={post.node} key={post.title}/>)}
         </div> 
         <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
-
+              <PostWidget />
+              <Categories />
             </div>
         </div>
       </div>
     </div>
   )
+}
+
+export async function getStaticProps()
+{
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts }
+  }
 }
