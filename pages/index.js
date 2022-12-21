378fa8e8b19db85a75816d2f0546d45aaa0136b9
Apurva Shah <shahh.apurva@gmail.com>
2022-12-20 19:34:10 -0800

    Initial Commit. Worked on some of the component structure and creating the home page.

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+const posts = [
+  {title: 'React Application', excerpt: 'We teach you how to create the react application'},
+  {title: 'Angular Application', excerpt: 'We teach you how to create the angular application'},
+];
+
+export default function Home() {
+  return (
+    <div className='container mx-auto px-10 mb-8 bg-gray-300'>
+      <Head>
+        <title>Apurva Shah's Blog</title>
+        <meta name="description" content="Sharing my journey through life!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div> 
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
