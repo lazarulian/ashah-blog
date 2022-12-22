7e985309e45a91851cd87a3be4dc07865c5b1593
Apurva Shah <shahh.apurva@gmail.com>
2022-12-21 18:15:57 -0800

    Finished the homepage. Added partial functionality for the actual posts including the image and general structure of the page

---
+++ components/Author.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Author = () => {
+  return (
+    <h1>
+        Author
+    </h1>
+  )
+}
+
+export default Author
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Link from 'next/link';
+import { getCategories } from '../services'
 
-const categories = [
-    {name: 'react', slug: 'react'},
-    {name: 'web dev', slug: 'web-dev'},
-]
+
 
 const Header = () => {
-  return (
+    const [categories, setCategories] = useState([]);
+    
+    useEffect(() => {
+        getCategories()
+          .then((newCategories) => setCategories(newCategories))
+      })
+    return (
     <div className="container mx-auto px-10 mb-8">
         <div className='border-b w-full inline-block border-blue-400 py-8'>
             <div className='md:float-left block'>
                 <Link href='/'>
                     <span className='transition duration-500 hover:text-yellow-400 cursor-pointer font-bold text-4xl text-white'>
-                        Apurva's Blog
+                        CheatCodes
                     </span>
                 </Link>
-
+                <br></br>
+                <span className='text-yellow-400 text-md font-semibold'>
+                    A blog developed and written by Apurva Shah
+                </span>
             </div>
             <div className='hidden md:float-left md:contents'>
                 {categories.map((category) => (
                     <Link key={category.slug} href = {`/category/${category.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'> 
+                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer transition duration-500 hover:text-yellow-400'> 
                             {category.name} 
                         </span>
                     </Link>
