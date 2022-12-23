592479cbad59c6b4e8cfe45fc58a75498175508b
Apurva Shah <shahh.apurva@gmail.com>
2022-12-23 13:44:56 -0800

    Added dynamic routing and query for category view

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { getPosts, getPostDetails } from "../../services";
 
-import { PostDetail, Categories, PostWidget, Author } from "../../components";
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Loader,
+} from "../../components";
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -39,6 +51,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   };
 }
