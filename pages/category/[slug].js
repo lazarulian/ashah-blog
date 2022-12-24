ba36136dcf2c3bbe59e30ac600be42a8de087add
Apurva Shah <shahh.apurva@gmail.com>
2022-12-23 22:31:16 -0800

    Style changes for the category specifically

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-
 import { getCategories, getCategoryPost } from "../../services";
-import { PostCard, Categories, Loader } from "../../components";
+import {
+  PostCard,
+  Categories,
+  Loader,
+  FeaturedPostCard,
+} from "../../components";
 
-const CategoryPost = ({ posts }) => {
+const CategoryPost = ({ posts, slug, params }) => {
   const router = useRouter();
-
   if (router.isFallback) {
     return <Loader />;
   }
@@ -15,9 +18,14 @@ const CategoryPost = ({ posts }) => {
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+            {posts.map((post, index) => (
+              // <PostCard key={index} post={post.node} />
+              <div className="w-64 h-64">
+                <FeaturedPostCard key={index} post={post.node} />
+              </div>
+            ))}
+          </div>
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
